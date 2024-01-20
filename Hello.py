0993eaed221111cff6a3860ea27a3ae9224a57e1
dinicolastefano <122374188+dinicolastefano@users.noreply.github.com>
2024-01-20 04:44:06 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -17,7 +17,19 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+chart_data = pd.DataFrame(
+   {
+       "col1": np.random.randn(20),
+       "col2": np.random.randn(20),
+       "col3": np.random.choice(["A", "B", "C"], 20),
+   }
+)
+
+st.area_chart(chart_data, x="col1", y="col2", color="col3")
 def run():
     st.set_page_config(
         page_title="Hello",
