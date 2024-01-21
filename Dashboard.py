4619084c434fec0cd283cce6d1c5d13a0820fb67
dinicolastefano <122374188+dinicolastefano@users.noreply.github.com>
2024-01-21 22:31:21 +0000

    fix auth

--- Dashboard.py
+++ Dashboard.py
@@ -8,13 +8,3 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-with open('../config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
