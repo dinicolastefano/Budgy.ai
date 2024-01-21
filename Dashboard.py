85f31e9dec3bb39f416f223685160e405792d716
dinicolastefano <122374188+dinicolastefano@users.noreply.github.com>
2024-01-21 15:56:03 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -3,7 +3,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import hiplot as hip
+data = [{'dropout':0.1, 'lr': 0.001, 'loss': 10.0, 'optimizer': 'SGD'},
+        {'dropout':0.15, 'lr': 0.01, 'loss': 3.5, 'optimizer': 'Adam'},
+        {'dropout':0.3, 'lr': 0.1, 'loss': 4.5, 'optimizer': 'Adam'}]
+hip.Experiment.from_iterable(data).display()
 
 # Title of our app
 st.title("Dashboard")
