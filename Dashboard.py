810a6bb0a6aee4a56108262766d8db40d13298d8
dinicolastefano <122374188+dinicolastefano@users.noreply.github.com>
2024-01-22 13:02:45 +0000

    layout

--- Dashboard.py
+++ Dashboard.py
@@ -9,6 +9,6 @@ row1 = st.columns(4)
 row2 = st.columns(1)
 row3= st.columns(2)
 
-for col in row1 + row2:
-    tile = col.container(height=120)
+for col in row1 :
+    tile = col.container(height=100)
     tile.title(":balloon:")
