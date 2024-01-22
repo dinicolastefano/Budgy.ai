62cbad193cb268c833ad49a85e547646a6131eba
dinicolastefano <122374188+dinicolastefano@users.noreply.github.com>
2024-01-22 06:46:11 +0000

    elements

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,38 @@
 # Import necessary libraries
 import streamlit as st
 import pandas as pd
+with elements("multiple_children"):
 
+    # You have access to Material UI icons using: mui.icon.IconNameHere
+    #
+    # Multiple children can be added in a single element.
+    #
+    # <Button>
+    #   <EmojiPeople />
+    #   <DoubleArrow />
+    #   Hello world
+    # </Button>
+
+    mui.Button(
+        mui.icon.EmojiPeople,
+        mui.icon.DoubleArrow,
+        "Button with multiple children"
+    )
+
+    # You can also add children to an element using a 'with' statement.
+    #
+    # <Button>
+    #   <EmojiPeople />
+    #   <DoubleArrow />
+    #   <Typography>
+    #     Hello world
+    #   </Typography>
+    # </Button>
+
+    with mui.Button:
+        mui.icon.EmojiPeople()
+        mui.icon.DoubleArrow()
+        mui.Typography("Button with multiple children")
 
 
 
